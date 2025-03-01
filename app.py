d742be61699f543dd9b4c67342bfbe791e067a20
Axiniform <alessc.surely573@simplelogin.com>
2025-03-01 12:19:55 -0500

    try/except block

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import logging
 from database import create_table
 from alerts import filter_MTA_alerts
 from calendar_scripts import build_and_post_events
 
 
+def main():
+    try:
+        create_table()
+        logging.info("Table created successfully")
+
+        fresh_MTA_alerts = filter_MTA_alerts('N')
+        logging.info("Filtered MTA alerts successfully")
+
+        build_and_post_events(fresh_MTA_alerts)
+        logging.info("Posted events to calendar successfully")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == '__main__':
-    create_table()
-
-    fresh_MTA_alerts = filter_MTA_alerts('N')
-
-    build_and_post_events(fresh_MTA_alerts)
-
+    main()
